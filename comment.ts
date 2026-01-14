14e21552948c2365997ca3591f7b5450754738af
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-01-14 21:38:45 +0900

    feat: integration of environmental variables

---
+++ comment.ts
@@ -0,0 +1,16 @@
+// Architectural pattern: MVC(Model-View-Controller), 
+//                  DI(Dependency Injection), MVP(Model View Presenter)
+
+// Design pattern: Middleware, Decorat
+
+// ========================================
+
+
+
+// Environmental Variables: 
+//                          Ayrim GIT HUB ga joylashtirilmaydigan
+// ma'lumotlarni aynan .env ga yozamiz! Misol uchun Database connection linki.
+// Bunday ma'lumotlar foydalanuvchilarga chiqib ketmasligi kerak!
+// .env dan foydalanishimiz uchun maxsus package kerak bo'ladi. >>> ("npm i dotenv")
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,2 @@
-console.log("EXECUTED!");
-
-import moment from 'moment';
-
-const currentTime = moment().format('YYYY MM DD');
-console.log(currentTime);
-
-const person: string = "Marco";
-const count: number = 100;
-
-// Architectural pattern: MVC(Model-View-Controller), 
-//                  DI(Dependency Injection), MVP(Model View Presenter)
-
-// Design pattern: Middleware, Decorat
+import dotenv from 'dotenv';
+dotenv.config();
