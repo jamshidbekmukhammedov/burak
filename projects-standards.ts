bd62b62ee744b0c0f334a995d171eec24a843ba1
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-01-20 00:03:22 +0900

    feat: create project standards

---
+++ projects-standards.ts
@@ -0,0 +1,8 @@
+/* Projects standards:
+   - Logging standards
+   - Logging standards
+        function, method, variable => CAMEL         goHome
+        class => KEBAB                              MemberService
+        css => SNAKE                                button_style
+   - ERROR handling     
+ */
