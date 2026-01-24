8658dd8f5990374b5be7b349b6c631c7eac72f28
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-01-24 14:21:04 +0900

    fix: modify restaurant login and signup process

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -2,6 +2,7 @@ import { MemberType } from '../libs/enums/member.enum';
 import MemberModel from '../schema/Member.model';
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors, { HttpCode, Message } from '../libs/Errors';
+import * as bcrypt from "bcryptjs";
 
 class MemberService {
     private readonly memberModel;
@@ -9,11 +10,16 @@ class MemberService {
         this.memberModel = MemberModel;
     }
 
+    // Signup qilish mantig'i!
     public async processSignup(input: MemberInput): Promise<Member> {
         const exist = await this.memberModel
             .findOne({MemberType: MemberType.RESTAURANT})
             .exec();
         if(exist) throw new Errors(HttpCode.BAD_REQUEST, Message.CREATED_FAILED); 
+
+        // Kirib kelayotgan Password ni BCRYPT orqali o'qib bo'lmas holatga o'zgartirilyapti!
+        const salt = await bcrypt.genSalt();
+        input.memberPassword = await bcrypt.hash(input.memberPassword, salt);
 
         try {
             const result = await this.memberModel.create(input);
@@ -24,6 +30,7 @@ class MemberService {
         }
     }
 
+    // Login qilish mantig'i!
     public async processLogin(input: LoginInput): Promise<Member> {
         const member = await this.memberModel
         .findOne(
@@ -32,8 +39,12 @@ class MemberService {
         )
         .exec();
         if(!member) throw new Errors(HttpCode.NOT_FOUND, Message.NO_MEMBER_NICK);
-
-        const isMatch = input.memberPassword === member.memberPassword;
+        // BCRYPT ni compare methodi orqali database dagi password bilan 
+        // input passwordni solishtiryapti!
+        const isMatch = await bcrypt.compare(
+            input.memberPassword, 
+            member.memberPassword);
+        
         if(!isMatch) {
             throw new Errors(HttpCode.UNAUTHORIZED, Message.WRONG_PASSWORD);
         }
