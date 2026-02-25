c53f7de175c5a2004806622dfc03dfc44ac1185e
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-02-25 21:55:07 +0900

    feat: develop verify authenticated user credentials

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,5 +1,4 @@
-import { AccessToken } from './../../node_modules/@aws-sdk/nested-clients/dist-types/submodules/signin/models/models_0.d';
-import { HttpCode } from './../libs/Errors';
+import { HttpCode, Message } from './../libs/Errors';
 import {Request, Response} from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
@@ -46,13 +45,30 @@ memberController.login = async (req: Request, res: Response) => {
             httpOnly: false,
         });
 
-        res.status(HttpCode.OK).json({ member: result, AccessToken: token });
+        res.status(HttpCode.OK).json({ member: result, accessToken: token });
     } catch (err) {
         console.log("Error, login:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart);
     }
 };
+
+memberController.verifyAuth = async (req: Request, res: Response) => {
+    try {
+        let member = null;
+        const token = req.cookies["accessToken"];
+        if(token) member = await authService.checkAuth(token);
+
+        if(!member) throw new Errors(HttpCode.UNAUTHORIZED, Message.NOT_AUTHENTICATED);
+
+        console.log("member:", member);
+        res.status(HttpCode.OK).json({ member: member });
+    } catch (err) {
+        console.log("Error, login:", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart);
+    }
+}
 
 
 export default memberController;
