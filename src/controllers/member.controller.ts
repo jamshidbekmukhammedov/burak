1170f4ca307ee33f2340dab9fccc583b5c45b066
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-02-25 21:05:48 +0900

    feat: develop authentication via tokens with cokkies

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -1,9 +1,12 @@
+import { AccessToken } from './../../node_modules/@aws-sdk/nested-clients/dist-types/submodules/signin/models/models_0.d';
+import { HttpCode } from './../libs/Errors';
 import {Request, Response} from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { LoginInput, Member, MemberInput } from "../libs/types/member";
 import Errors from "../libs/Errors";
 import AuthService from "../models/Auth.service";
+import { AUTH_TIMER } from "../libs/config";
 
 
 // REACT
@@ -15,12 +18,15 @@ memberController.signup = async (req: Request, res: Response) => {
     try{
         console.log("signup");
         const input:MemberInput = req.body,
-         result: Member = await memberService.signup(input);
-         const token = await authService.createToken(result);
-         console.log("token:", token);
+            result: Member = await memberService.signup(input);
+            const token = await authService.createToken(result);
+        
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000, 
+            httpOnly: false,
+        });
 
-
-        res.json({ member: result });
+        res.status(HttpCode.CREATED).json({ member: result, AccessToken: token });
     } catch (err) {
         console.log("Error, signup:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
@@ -32,11 +38,15 @@ memberController.login = async (req: Request, res: Response) => {
     try{
         console.log("login");
         const input: LoginInput = req.body,
-         result = await memberService.login(input),
-         token = await authService.createToken(result);
-         console.log("token:", token);
+            result = await memberService.login(input),
+            token = await authService.createToken(result);
+        
+        res.cookie("accessToken", token, {
+            maxAge: AUTH_TIMER * 3600 * 1000, 
+            httpOnly: false,
+        });
 
-        res.json({ member: result });
+        res.status(HttpCode.OK).json({ member: result, AccessToken: token });
     } catch (err) {
         console.log("Error, login:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
