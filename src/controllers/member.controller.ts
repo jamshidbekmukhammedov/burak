0199dea72f9bf431be15066607f004de257a43ed
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-02-28 01:13:06 +0900

    feat: develop updateMember business logic

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -2,7 +2,7 @@ import { HttpCode, Message } from './../libs/Errors';
 import {NextFunction, Request, Response} from "express";
 import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
-import { ExtendedRequest, LoginInput, Member, MemberInput } from "../libs/types/member";
+import { ExtendedRequest, LoginInput, Member, MemberInput, MemberUpdateInput } from "../libs/types/member";
 import Errors from "../libs/Errors";
 import AuthService from "../models/Auth.service";
 import { AUTH_TIMER } from "../libs/config";
@@ -73,6 +73,21 @@ memberController.getMemberDetail = async (req: ExtendedRequest, res: Response) =
         res.status(HttpCode.OK).json(result);
     } catch (err) {
         console.log("Error, getMemberDetail:", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart);
+    }
+};
+
+memberController.updateMember = async (req: ExtendedRequest, res: Response) => {
+    try{
+        console.log("updateMember");
+        const input: MemberUpdateInput =req.body;
+        if(req.file) input.memberImage = req.file.path.replace(/\\/, "/");
+        const result = await memberService.updateMember(req.member, input);
+
+        res.status(HttpCode.OK).json(result);
+    } catch (err) {
+        console.log("Error, updateMember:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart);
     }
