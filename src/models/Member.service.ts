d660ffe009261bd8d1c448bfba78fea335133131
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-02-28 14:57:25 +0900

    feat: develop getRestaurant rest API

--- src/models/Member.service.ts
+++ src/models/Member.service.ts
@@ -13,6 +13,16 @@ class MemberService {
     }
 
     /* SPA*/ 
+    public async getRestaurant(): Promise<Member> {
+        const result = await this.memberModel
+        .findOne({memberType: MemberType.RESTAURANT})
+        .lean()
+        .exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        return result;
+    }
+
     // Signup qilish mantig'i
     public async signup(input: MemberInput): Promise<Member> {
         const salt = await bcrypt.genSalt();
