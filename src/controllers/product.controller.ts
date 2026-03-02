e16a06f3adbe448e6b32fa08504ec58c5df311d3
Jamshidbek <jamshidbekmukhammedov@gmail.com>
2026-03-03 01:17:46 +0900

    feat: develop getProduct rest API

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,7 +4,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { T } from "../libs/types/common";
 import ProductService from "../models/Product.service";
 import { ProductInput } from "../libs/types/product";
-import { AdminRequest } from "../libs/types/member";
+import { AdminRequest, ExtendedRequest } from "../libs/types/member";
 import { ProductCollection } from '../libs/enums/product.enum';
 
 const productService = new ProductService();
@@ -31,6 +31,22 @@ productController.getProducts = async (req: Request, res: Response) => {
         res.status(HttpCode.OK).json(result);
     } catch (err) {
         console.log("Error, getProducts:", err);
+        if(err instanceof Errors) res.status(err.code).json(err);
+        else res.status(Errors.standart.code).json(Errors.standart);
+    }
+}
+
+productController.getProduct = async (req: ExtendedRequest, res: Response) => {
+    try{
+        console.log("getProduct");
+        
+        const { id } = req.params;
+        const memberId = req.member?._id ?? null,
+            result = await productService.getProduct(memberId, id as string);
+
+        res.status(HttpCode.OK).json(result);    
+    } catch (err) {
+        console.log("Error, getProduct:", err);
         if(err instanceof Errors) res.status(err.code).json(err);
         else res.status(Errors.standart.code).json(Errors.standart);
     }
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -4,6 +4,7 @@ import Errors, { HttpCode, Message } from "../libs/Errors";
 import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import ProductModel from "../schema/Product.model";
 import { ProductStatus } from "../libs/enums/product.enum";
+import { ObjectId } from "mongoose";
 
 class ProductService {
     private readonly productModel;
@@ -22,7 +23,7 @@ class ProductService {
         if(inquiry.search) {
             match.productName = {$regex: new RegExp(inquiry.search, "i")};
         }
-        
+
         const sort: T = 
         inquiry.order === "productPrice" 
         ? {[inquiry.order]: 1} 
@@ -36,6 +37,25 @@ class ProductService {
         ])
         .exec();
         if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+        return result;
+    }
+
+    public async getProduct(
+        memberId: ObjectId | null, 
+        id: string
+    ): Promise<Product> {
+        const productId = shapeIntoMongooseObjectId(id);
+
+        let result = await this.productModel
+            .findOne({
+                _id: productId, 
+                productStatus: ProductStatus.PROCESS
+            })
+            .exec();
+        if(!result) throw new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+        // TODO: If authenticated users => first => view log creation
+
         return result;
     }
 
